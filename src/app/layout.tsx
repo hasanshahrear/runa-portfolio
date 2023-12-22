73aeb525c2fb70a60b58bb8f5eedd0dba2fac314
Hasan Shahrear <hasanshahrear143@gmail.com>
2023-12-22 21:25:28 +0600

    tried to fix build issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,16 @@
-import { GlobalProvider } from '@/features/providers'
-import type { Metadata } from 'next'
-import { Open_Sans } from 'next/font/google'
+"use client"
 
+import { GlobalProvider } from '@/features/providers';
+// import type { Metadata } from 'next';
+import { Open_Sans } from 'next/font/google';
+import "primereact/resources/themes/lara-light-cyan/theme.css";
 
 const openSans = Open_Sans({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: "Rejuna's Portfolio",
-  description: 'This is a personal portfolio',
-}
+// export const metadata: Metadata = {
+//   title: "Rejuna's Portfolio",
+//   description: 'This is a personal portfolio',
+// }
 
 type TProps = {
   children: React.ReactNode
