f628258c834308dfe3a668e6295cee09070efe1d
Hasan Shahrear <hasanshahrear143@gmail.com>
2023-12-22 21:21:44 +0600

    tried to fix build issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { GlobalProvider } from '@/features/providers'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
-import './globals.css'
+
 
 const openSans = Open_Sans({ subsets: ['latin'] })
 
