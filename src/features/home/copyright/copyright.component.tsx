41801494faf43e64fd5de216e8f98699aad19e22
Hasan Shahrear <hasanshahrear143@gmail.com>
2023-12-25 19:02:30 +0600

    copyright responsive done

--- src/features/home/copyright/copyright.component.tsx
+++ src/features/home/copyright/copyright.component.tsx
@@ -4,15 +4,12 @@ import { FaBehance, FaInstagram, FaLinkedinIn, FaTwitter } from "react-icons/fa"
 export function Copyright(){
     return (
         <div className=" bg-[#DFFCF259] py-5">
-            <div className="container grid grid-cols-12">
-                <div className="col-span-3">
+            <div className="container grid grid-cols-12 gap-5">
+                <div className="col-span-12 md:col-span-6 text-center lg:text-left">
                     <p className=" text-lg">Copyright © 2023 | All right to Reserved</p>
                 </div>
-                <div className="col-span-6 text-center">
-                    
-                </div>
-                <div className="col-span-3">
-                    <div className="space-x-4 text-right">
+                <div className="col-span-12 md:col-span-6">
+                    <div className="space-x-4 text-center md:text-right">
                         <Link href="#" className=" inline-block text-purple" aria-label="LinkedinIn">
                             <FaLinkedinIn size="30" />
                         </Link>
