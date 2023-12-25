d0eb5452767db7aeca48f7ad21b003ed11030f83
Hasan Shahrear <hasanshahrear143@gmail.com>
2023-12-25 18:48:58 +0600

    client done

--- src/features/home/clients/clients.component.tsx
+++ src/features/home/clients/clients.component.tsx
@@ -5,7 +5,7 @@ export function Clients() {
         <div className=" bg-[#DFFCF259] pt-28 pb-44">
             <div className="container grid grid-cols-12 mb-20">
                 <div className="col-span-3"></div>
-                <div className="col-span-6 text-center">
+                <div className="col-span-12 lg:col-span-6 text-center">
                     <Heading>My Clients Love</Heading>
                     <p>
                         Lorem ipsum dolor sit amet consectetur. Ultrices amet at varius proin nec felis purus adipiscing. Risus mauris pellentesque interdum phasellus fermentum. Sed porta aliquam in egestas. Diam eros vulputate convallis nunc justo integer non.
