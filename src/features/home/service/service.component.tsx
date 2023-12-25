040aafc26eec120a6cdb9cf3b108d54cc24f45d1
Hasan Shahrear <hasanshahrear143@gmail.com>
2023-12-25 18:30:24 +0600

    service responsive done

--- src/features/home/service/service.component.tsx
+++ src/features/home/service/service.component.tsx
@@ -6,7 +6,7 @@ export function ServiceSection(){
         <div className=" bg-[#DFFCF259] py-28">
             <div className="container grid grid-cols-12 mb-20">
                 <div className="col-span-3"></div>
-                <div className="col-span-6 text-center">
+                <div className="col-span-12 lg:col-span-6 text-center">
                     <Heading>My Awesome Service</Heading>
                     <p>
                         Lorem ipsum dolor sit amet consectetur. Ultrices amet at varius proin nec felis purus adipiscing. Risus mauris pellentesque interdum phasellus fermentum. Sed porta aliquam in egestas. Diam eros vulputate convallis nunc justo integer non.
@@ -14,21 +14,21 @@ export function ServiceSection(){
                 </div>
             </div>
             <div className="container grid grid-cols-12 gap-6">
-                <div className="col-span-4 space-y-5 card hover:card-hover text-center">
+                <div className="col-span-12 md:col-span-6 lg:col-span-4 space-y-5 card hover:card-hover text-center">
                     <Image src="/img/ui-ux-design.svg" height="136" width="167" alt="ui ux design" className="mx-auto" />
                     <Heading className=" text-3xl">UI/UX Design</Heading>
                     <p>
                         Lorem ipsum dolor sit amet consectetur. Cursus diam dignissim feugiat ac nullam egestas semper ultricies nam.
                     </p>
                 </div>
-                <div className="col-span-4 space-y-5 card hover:card-hover text-center">
+                <div className="col-span-12 md:col-span-6 lg:col-span-4 space-y-5 card hover:card-hover text-center">
                     <Image src="/img/web-development.svg" height="136" width="167" alt="ui ux design" className="mx-auto" />
                     <Heading className=" text-3xl">Web Development</Heading>
                     <p>
                         Lorem ipsum dolor sit amet consectetur. Cursus diam dignissim feugiat ac nullam egestas semper ultricies nam.
                     </p>
                 </div>
-                <div className="col-span-4 space-y-5 card hover:card-hover text-center">
+                <div className="col-span-12 md:col-span-6 lg:col-span-4 space-y-5 card hover:card-hover text-center">
                     <Image src="/img/app-design.svg" height="136" width="167" alt="ui ux design" className="mx-auto" />
                     <Heading className=" text-3xl">App Design</Heading>
                     <p>
